bc713d01fb78d78f6fb2d9fc6235d80833657cc9
Cristian Rozas <cristianrozas.cr@gmail.com>
2024-03-09 16:48:31 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,1 @@
+var bordes = false;
---
+++ assets/js/item-2.js
@@ -0,0 +1,7 @@
+// let primerSticker = document.querySelector("#sticker-1").value
+// let segundoSticker = document.querySelector("#sticker-2").value
+// let tercerSticker = document.querySelector("#sticker-3").value
+
+// console.log(typeof primerSticker)
+// console.log(typeof segundoSticker)
+// console.log(typeof tercerSticker)
